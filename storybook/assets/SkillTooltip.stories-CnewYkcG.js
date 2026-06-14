cfe320b0a57eed20b7f041b96eeaf34dd5636f4a
BraydenPB <BraydenPB@users.noreply.github.com>
2026-06-14 07:14:12 +0000

    deploy: ESO-Toolkit/eso-toolkit@af77f1efc90dfdc31697287fe88d63cb3c19a2ba

---
+++ storybook/assets/SkillTooltip.stories-CnewYkcG.js
@@ -0,0 +1,20 @@
+import{o as e}from"./preload-helper-CHxnduP2.js";import{SkillTooltip as t,t as n}from"./SkillTooltip-B5cpVPJw.js";var r,i,a,o,s;e((()=>{n(),r={title:`Components/SkillTooltip`,component:t,parameters:{layout:`centered`}},i=[{label:`Cost`,value:`4455 Magicka`},{label:`Target`,value:`Self`},{label:`Duration`,value:`30 seconds`}],a={args:{headerBadge:`Active`,lineText:`Arcanist — Herald of the Tome`,abilityId:185842,name:`Inspired Scholarship`,morphOf:`Tome-Bearer's Inspiration`,stats:i,description:`Etch a series of runes onto your weapon that pulse with power once every 3 seconds. Each pulse enhances your class abilities, and striking an enemy with one deals an additional 935 Magic Damage and generates Crux if you have none. While slotted on either ability bar, gain Major Brutality and Major Sorcery, increasing your Weapon and Spell Damage by 20%.`}},o={args:{headerBadge:`Passive`,lineText:`Arcanist — Herald of the Tome`,abilityId:196226,name:`Tome-Bearer's Inspiration`,stats:[],description:`While slotted, gain Major Brutality and Major Sorcery, increasing Weapon and Spell Damage by 20%.`}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    headerBadge: 'Active',
+    lineText: 'Arcanist — Herald of the Tome',
+    abilityId: 185842,
+    name: 'Inspired Scholarship',
+    morphOf: "Tome-Bearer's Inspiration",
+    stats,
+    description: 'Etch a series of runes onto your weapon that pulse with power once every 3 seconds. Each pulse enhances your class abilities, and striking an enemy with one deals an additional 935 Magic Damage and generates Crux if you have none. While slotted on either ability bar, gain Major Brutality and Major Sorcery, increasing your Weapon and Spell Damage by 20%.'
+  }
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    headerBadge: 'Passive',
+    lineText: 'Arcanist — Herald of the Tome',
+    abilityId: 196226,
+    name: "Tome-Bearer's Inspiration",
+    stats: [],
+    description: 'While slotted, gain Major Brutality and Major Sorcery, increasing Weapon and Spell Damage by 20%.'
+  }
+}`,...o.parameters?.docs?.source}}},s=[`ActiveSkill`,`PassiveSkill`]}))();export{a as ActiveSkill,o as PassiveSkill,s as __namedExportsOrder,r as default};
